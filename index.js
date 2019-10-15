61f9cf679ad9ea199c14b25224c2d710719a6ad9
Chad Kirby <chad.kirby@gmail.com>
2019-10-15 16:49:10 -0700

    enable regex literal syntax

--- index.js
+++ index.js
@@ -1,41 +1,3 @@
-
-// makeTagProcessor is a helper function to roll your own tag processor function
-// eg, if you wanted to URI-encode substitution values in a template string,
-// you could do something like so:
-// let enc = makeTagProcessor((index, literals, cookedValues) => literals[index] + encodeURIComponent(cookedValues[index] || ''));
-// let xx = 'hi there';
-// console.log(enc`<span>${xx}</span>`);
-// <span>hi%20there</span>
-// or, using the postProcessor option:
-// let $enc = makeTagProcessor(
-//     (index, literals, cookedValues) => literals[index] + encodeURIComponent(cookedValues[index] || ''),
-//     (str) => Ember.$(str)
-// );
-// let xx = 'hi there';
-// console.log($enc`<span>${xx}</span>`);
-// [ <span>hi%20there</span> ]
-
-function makeTagProcessor(tagFn, postProcessor) {
-  return function(literals, ...cookedValues) {
-    let out = ``;
-    for (const index of literals.keys()) {
-      out += tagFn(index, literals, cookedValues);
-    }
-    if (postProcessor) {
-      return postProcessor(out);
-    }
-    return out;
-  };
-}
-
-function escapeForRegex(str) {
-  return str.toString().replace(/[-[\]/{}()*+?.\\^$|]/g, "\\$&");
-}
-
-function isNone(obj) {
-  return obj === null || obj === undefined;
-}
-
 // coffeescript block regular expressions are awesome http://coffeescript.org/#regexes
 // > Similar to block strings… CoffeeScript supports block regexes — extended regular
 // > expressions that ignore internal whitespace and can contain comments and interpolation.
@@ -48,36 +10,77 @@ function isNone(obj) {
 // NB, in the chrome debugger, you have to assign the tag processor fn to a value before you can use it
 // re = blockRE('g'); re`( a )`;
 // gives you /a/g
-function blockRE(flags, options) {
-  if (typeof(flags) === 'object' && isNone(options)) {
+function blockRE(...args) {
+  let escapeText;
+  let flags;
+  let options;
+
+  // check whether this function is being used directly as a tag function
+  if (args[0] && Array.isArray(args[0]) && args[0].raw) {
+    escapeText = false;
+    // in this mode, the raw literals may have literal RegExp slashes & trailing
+    // flags
+    let [ literals, ...cookedValues ] = args;
+    // make an unfrozen copy of the raw literals
+    let raw = literals.raw.slice();
+    let last = raw[raw.length - 1];
+    let trailing = last.match(/[/][gimsuye]*$/);
+    if (raw[0].startsWith('/') && trailing) {
+      // the tagged string has literal RegExp syntax
+      // grab the flags (if any)
+      flags = trailing[0].slice(1);
+      if (/e/.test(flags)) {
+        escapeText = true;
+        flags = flags.replace(/e/g, '');
+      }
+      // remove the trailing '/' & flags (if any)
+      raw[raw.length - 1] = last.slice(0, -trailing[0].length);
+      // remove the leading '/
+      raw[0] = raw[0].slice(1);
+    }
+    return tagFn({ raw }, ...cookedValues);
+  }
+
+  // else return a tagged template function with the backwards-compatible
+  // flags & options
+  [ flags = '', options ] = args;
+  if (typeof(flags) === 'object' && options === undefined) {
     options = flags;
     flags = '';
   }
-  options = options || { escapeText: true };
-  return makeTagProcessor(function(index, literals, cookedValues) {
-    let cooked = cookedValues[index];
-    // replace white-space and comments in the raw string, allowing escaped '/' and whitespace
-    // but also match the escaped escape /\\\\/, so it doesn't act like an escape!
-    let literal = literals.raw[index].replace(/(\\\\|\\\/|\\\s)|\s+(?:\/\/.*)?/g, '$1');
-    if (index === cookedValues.length) {
-      return literal;
+  ({ escapeText = true } = options || {});
+
+  return tagFn;
+
+  function tagFn({ raw: literals }, ...cookedValues) {
+    let out = ``;
+    for (const i of literals.keys()) {
+      let literal = literals[i];
+      // replace white-space and comments in the raw string, allowing escaped '/'
+      // and whitespace but also match the escaped escape /\\\\/, so it doesn't
+      // act like an escape!
+      out += literal.replace(/(\\\\|\\\/|\\\s)|\s+(?:\/\/.*)?/g, '$1');
+
+      let cooked = cookedValues[i];
+      if (cooked) {
+        if (cooked.source) {
+          // if the cooked substitution is a regex, then include its source
+          cooked = cooked.source;
+        } else if (escapeText) {
+          // escape the substitution as a string when the substitution is not a
+          // regex, and we are escaping text
+          cooked = String(cooked).replace(
+            /[-[\]/{}()*+?.\\^$|]/g,
+            "\\$&"
+          );
+        }
+        out += cooked;
+      }
     }
-    // this bit differs from/improves on the coffeescript block regex (which just substitutes
-    // everything as a string, including regexes) because the coffee compiler just twiddles syntax
-    if (cooked.source) {
-      // if the cooked substitution is a regex, then include its source; <= whoopie
-      cooked = cooked.source;
-    } else if (options.escapeText) {
-      // else if the substitution is not a regex, and we are escaping text
-      // then escape the substitution as a string
-      cooked = escapeForRegex(cooked);
-    }
-    return `${literal}${cooked}`;
-  },
-  // postProcessor: after assembling the regex string, turn it into a RegExp
-  function(reString) {
-    return new RegExp(reString, flags || '');
-  });
+
+    return new RegExp(out, flags);
+  }
+
 }
 
 module.exports = blockRE;
--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 const test = require('tape');
-const BLOCKRE = require('./index');
+const RE = require('./index');
 
 test(`blockRE works`, (assert) => {
-  const re = BLOCKRE();
+  const re = RE();
   assert.ok(re`` instanceof RegExp);
   assert.equal(re`a`.source, `a`);
   assert.ok(re`a`.test('abc'));
@@ -10,27 +10,32 @@ test(`blockRE works`, (assert) => {
 });
 
 test(`blockRE accepts flags`, (assert) => {
-  assert.equal(BLOCKRE()``.flags, ``);
-  assert.equal(BLOCKRE('g')``.flags, `g`);
-  assert.equal(BLOCKRE('i')``.flags, `i`);
-  assert.equal(BLOCKRE('m')``.flags, `m`);
-  assert.equal(BLOCKRE('gim')``.flags, `gim`);
+  assert.equal(RE()``.flags, ``);
+  assert.equal(RE('g')``.flags, `g`);
+  assert.equal(RE('i')``.flags, `i`);
+  assert.equal(RE('m')``.flags, `m`);
+  assert.equal(RE('gim')``.flags, `gim`);
   assert.end();
 });
 
 test(`blockRE ignores whitespace`, (assert) => {
-  const re = BLOCKRE();
+  const re = RE();
   assert.equal(re`a b c`.source, `abc`);
   assert.ok(re`
     a
     b
     c
   `.test('abc'));
+  assert.equal(
+    re`a\ *b\ *c`.source,
+    `a\\ *b\\ *c`,
+    `escaped whitespace is honored`
+  );
   assert.end();
 });
 
 test(`blockRE ignores comments`, (assert) => {
-  const re = BLOCKRE();
+  const re = RE();
   assert.equal(re`
     a // match a literal 'a'
   `.source, `a`);
@@ -43,22 +48,22 @@ test(`blockRE ignores comments`, (assert) => {
 });
 
 test(`blockRE doesn't require escaping special chars`, (assert) => {
-  const re = BLOCKRE();
+  const re = RE();
   assert.equal(re`\d`.source, `\\d`);
   assert.ok(re`\w{3}`.test('abc'));
   assert.end();
 });
 
 test(`blockRE does substitutions`, (assert) => {
-  const re = BLOCKRE();
-  const A = /[aA]/;
-  assert.equal(re`${A}${'bc'}`.source, `[aA]bc`);
-  assert.ok(re`${A}${'bc'}`.test('abc'));
+  const re = RE();
+  const A = /a/;
+  assert.equal(re`${A}${'b'}c`.source, `abc`);
+  assert.ok(re`\s*${A}${'bc'}`.test('abc'));
   assert.end();
 });
 
 test(`blockRE escapes string substitutions, not regex substitutions`, (assert) => {
-  const re = BLOCKRE();
+  const re = RE();
   const A = /[aA]/; // inserted as regex
   const B = `[bB]`; // inserted as escaped string
   assert.equal(re`${A}${B}`.source, `[aA]\\[bB\\]`);
@@ -70,8 +75,34 @@ test(`blockRE escapes string substitutions, not regex substitutions`, (assert) =
   );
 
   assert.equal(
-    BLOCKRE({ escapeTest: false })`${C}${C.source}`.source,
+    RE('', { escapeText: false })`${C}${C.source}`.source,
     `[\\w\\W][\\w\\W]`
+  );
+  assert.equal(
+    RE({ escapeText: false })`${C}${C.source}`.source,
+    `[\\w\\W][\\w\\W]`
+  );
+  assert.end();
+});
+
+test(`BLOCKRE accepts regex literal syntax`, (assert) => {
+  assert.equal(RE`/x/g`.source, `x`);
+  assert.equal(RE`/
+    y // match 'y'
+  /g`.source, `y`);
+  assert.equal(RE`/x/g`.flags, `g`);
+  assert.equal(RE`/x/suygim`.flags, `gimsuy`);
+  assert.equal(RE`/ a ${'b'} ${/c/} /`.source, `abc`);
+  assert.equal(RE`/ ${/[a]/} /`.source, `[a]`);
+  assert.equal(
+    RE`/ ${/[a]\d{1}/.source} /`.source,
+    `[a]\\d{1}`,
+    `don't escape text by default`
+  );
+  assert.equal(
+    RE`/ ${/[a]\d{1}/.source} /e`.source,
+    `\\[a\\]\\\\d\\{1\\}`,
+    `'e' flag opts in to escape text in substitutions`
   );
   assert.end();
 });
